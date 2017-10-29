7c96f8b8c6b68cc9f5db946f6bb46ab5e7453efb
Ilya Kaysin <demarkok@gmail.com>
2017-10-29 14:38:00 +0300

    add tests

--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -1,10 +1,48 @@
 package ru.spbau.mit
-import kotlin.test.assertEquals
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class TestSource {
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun sampleTest1() {
+        val k = 2
+        val string = "bac"
+
+        val queries: List<NameEditor.Query> = listOf<NameEditor.Query>(
+                NameEditor.Query(2, 'a'),
+                NameEditor.Query(1, 'b'),
+                NameEditor.Query(2, 'c'))
+
+        assertEquals("acb", solve(k, string, queries))
+    }
+
+    @Test
+    fun sampleTest2() {
+        val k = 1
+        val string = "abacaba"
+
+        val queries: List<NameEditor.Query> = listOf<NameEditor.Query>(
+                NameEditor.Query(1, 'a'),
+                NameEditor.Query(1, 'a'),
+                NameEditor.Query(1, 'c'),
+                NameEditor.Query(2, 'b'))
+
+        assertEquals("baa", solve(k, string, queries))
+    }
+
+    @Test
+    fun sampleTest3() {
+        val k = 4
+        val string = "db"
+
+        val queries: List<NameEditor.Query> = listOf<NameEditor.Query>(
+                NameEditor.Query(1, 'd'),
+                NameEditor.Query(2, 'd'),
+                NameEditor.Query(2, 'b'),
+                NameEditor.Query(1, 'd'),
+                NameEditor.Query(2, 'b'))
+
+        assertEquals("bdb", solve(k, string, queries))
     }
 }
